0b05cecca92d6b62b74d602ba1a125346c73198e
Chris Stucchio <stucchio@gmail.com>
2012-10-01 23:45:38 -0400

    Added convenience functions

--- src/main/scala/tiramisu/Tiramisu.scala
+++ src/main/scala/tiramisu/Tiramisu.scala
@@ -9,21 +9,8 @@ class DuplicatedParameterException(e: String) extends TiramisuException(e)
 class InvalidParameterException(e: String) extends TiramisuException(e)
 class UnusedParameterException(e: String) extends TiramisuException(e)
 
-class Query(val sql: String, val params: Map[String,Any]) {
-  def add(q: Query): Query = {
-    val finalSql = sql + q.sql
-    val paramIntersection = params.keySet & q.params.keySet
-    val duplicatedParameters = paramIntersection.filter( k => params(k) != q.params(k) )
-
-    if (!(params.keySet ++ q.params.keySet).forall(p => Query.parameterIsValid(p))) { //Make sure all parameters valid
-      throw new InvalidParameterException("Invalid parameters: " + (params.keySet ++ q.params.keySet).filter(p => !Query.parameterIsValid(p)) )
-    }
-
-    if (!duplicatedParameters.isEmpty) { //We can't combine queries where both have the same parameter key, but the value is different
-      throw new DuplicatedParameterException("Cannot combine queries, contain duplicated parameters for which the values differ: " + duplicatedParameters.map(k => (params(k), q.params(k))))
-    }
-    new Query(sql + q.sql, params ++ q.params)
-  }
+class Query(val sql: String, val params: Map[String,anorm.ParameterValue[_]]) {
+  def add(q: Query): Query = new Query(sql + q.sql, mergeParams(params, q.params))
 
   override def equals(other: Any) = {
     other match {
@@ -31,11 +18,46 @@ class Query(val sql: String, val params: Map[String,Any]) {
       case _ => false
     }
   }
+
+  def AND(q: Query) = addWithCenter(" AND ", this, q)
+  def OR(q: Query) = addWithCenter(" OR ", this, q)
+  def WHERE(q: Query) = addWithCenter(" WHERE ", this, q)
+
+  def formatS(queries: Query*) = new Query(sql.format(queries: _*),
+					   queries.foldLeft(params)( (x:Map[String,anorm.ParameterValue[_]],y:Query) => mergeParams(x, y.params)))
+  def formatV(values: anorm.ParameterValue[_]*) = {
+    val newParams = values.map(x => (Query.randomParam, x))
+    new Query( sql.format(newParams.map(x => "{%s}".format(x._1)): _*), mergeParams(params, newParams.toMap))
+  }
+
+  private def addWithCenter(center: String, q: Query, p: Query): Query = new Query(q.sql + center + p.sql, mergeParams(q.params, p.params))
+
+  private def mergeParams(p: Map[String, anorm.ParameterValue[_]], q: Map[String, anorm.ParameterValue[_]]): Map[String, anorm.ParameterValue[_]] = {
+    val paramIntersection = p.keySet & q.keySet
+    val duplicatedParameters = paramIntersection.filter( k => p(k) != q(k) )
+
+    if (!(p.keySet ++ q.keySet).forall(x => Query.parameterIsValid(x))) { //Make sure all parameters valid
+      throw new InvalidParameterException("Invalid parameters: " + (p.keySet ++ q.keySet).filter(x => !Query.parameterIsValid(x)) )
+    }
+
+    if (!duplicatedParameters.isEmpty) { //We can't combine queries where both have the same parameter key, but the value is different
+      throw new DuplicatedParameterException("Cannot combine queries, contain duplicated parameters for which the values differ: " + duplicatedParameters.map(k => (p(k), q(k))))
+    }
+    p ++ q
+  }
 }
 
 object Query {
   private val parameterRegex = java.util.regex.Pattern.compile("^[\\w]+\\z")
   def parameterIsValid(p: String) = parameterRegex.matcher(p).matches()
+  def randomParam = java.util.UUID.randomUUID().toString().replace('-','_')
+}
+
+object Tiramisu {
+  import anorm._
+  def Select(q: Query)(implicit connection: java.sql.Connection) = SQL(q.sql).on(q.params.toSeq: _*)()(connection)
+  def Insert(q: Query)(implicit connection: java.sql.Connection) = SQL(q.sql).on(q.params.toSeq: _*).executeInsert()(connection)
+  def Update(q: Query)(implicit connection: java.sql.Connection) = SQL(q.sql).on(q.params.toSeq: _*).executeUpdate()(connection)
 }
 
 object Syntax {
@@ -45,21 +67,21 @@ object Syntax {
   }
 
   class StringToQuery(s: String) {
-    def sql: Query = new Query(s, Map[String,Any]())
+    def sql: Query = new Query(s, Map[String,anorm.ParameterValue[_]]())
 
-    def sqlP(params: (String,Any)*): Query = new Query(s, params.toMap)
-    def sqlP(params: Map[String,Any]): Query = new Query(s, params)
+    def sqlP(params: (String,anorm.ParameterValue[_])*): Query = new Query(s, params.toMap)
+    def sqlM(params: Map[String,anorm.ParameterValue[_]]): Query = new Query(s, params)
   }
 
   implicit def stringToStringToQuery(s: String): StringToQuery = new StringToQuery(s)
 
-  class ValToParam(x: Any) {
+  class ValToParam(x: anorm.ParameterValue[_]) {
     def sqlV: Query = {
-      val paramName = java.util.UUID.randomUUID().toString().replace('-','_')
-      new Query("{%s}".format(paramName), Map[String,Any](paramName -> x))
+      val paramName = Query.randomParam
+      new Query("{%s}".format(paramName), Map[String,anorm.ParameterValue[_]](paramName -> x))
     }
   }
 
-  implicit def valToParam(x: Any):ValToParam = new ValToParam(x)
+  implicit def valToParam(x: anorm.ParameterValue[_]):ValToParam = new ValToParam(x)
 
 }
--- src/test/scala/BaseTest.scala
+++ src/test/scala/BaseTest.scala
@@ -2,6 +2,7 @@ import org.scalacheck._
 
 import scalaz._
 import Scalaz._
+import anorm._
 
 import Prop.forAll
 
@@ -10,58 +11,74 @@ import com.chrisstucchio.tiramisu.Syntax._
 
 object Generators {
   val legitimateString = Gen.alphaStr suchThat (_.size > 0)
+
   val alphaMap = for {
     x <- Gen.containerOf[Set,String](legitimateString)
-  } yield (x.map( t => (t, t+t) ).toMap)
+  } yield (x.map( t => (t, toParameterValue(t+t)) ).toMap)
 
   val genQuery = for {
     sql <- legitimateString
     params <- alphaMap
   } yield new Query(sql, params)
 
+  def elementsInAtLeastTwoSets(m: Seq[Map[String,_]]): Set[String] = {
+    val pairsOfMaps = for {
+        x <- m
+        y <- m
+      } yield (x,y)
+    val duplicatedElements = pairsOfMaps.map( t => t._1.keySet intersect t._2.keySet ).fold(Set())( (a,b) => a union b)
+    duplicatedElements
+  }
+
+  def noDupes[A](a: Map[String,A], b: Map[String,A]): (Map[String,A], Map[String,A]) = (a -- (a.keySet intersect b.keySet), b -- (a.keySet intersect b.keySet))
 }
 
 object QuerySpecificationMonoid extends Properties("Query.MonoidLaws") {
   import Generators._
-  property("equality") = forAll(legitimateString, alphaMap) { (a: String, b: Map[String,String]) => {
-    a.sqlP(b) == a.sqlP(b)
+  property("equality") = forAll(legitimateString, alphaMap) { (a: String, b: Map[String,ParameterValue[_]]) => {
+    a.sqlP(b.toSeq: _*) == a.sqlP(b.toSeq: _*)
   } }
 
-  property("zero") = forAll(legitimateString, alphaMap) { (a: String, b: Map[String,String]) => {
-    a.sqlP(b) == (a.sqlP(b) |+| "".sql)
+  property("zero") = forAll(legitimateString, alphaMap) { (a: String, b: Map[String,ParameterValue[_]]) => {
+    a.sqlM(b) == (a.sqlM(b) |+| "".sql)
   } }
 
   property("associativity") = forAll(genQuery, genQuery, genQuery) { (a: Query, b: Query, c: Query) => {
-    ((a |+| b) |+| c) == (a |+| (b |+| c))
+    //First strip out duplicated keys
+    val dupKeys = elementsInAtLeastTwoSets(Seq(a.params, b.params, c.params))
+    val ap, bp, cp = (new Query(a.sql, a.params -- dupKeys), new Query(b.sql, b.params -- dupKeys), new Query(c.sql, c.params -- dupKeys))
+    ((ap |+| bp) |+| cp) == (ap |+| (bp |+| cp))
   } }
 }
 
 object QuerySpecification extends Properties("Query.NormalUse") {
-  import Prop.forAll
+  import Prop._
 
   import Generators._
 
-  property("params are set properly") = forAll { (a: Map[String,String]) => (new Query("SELECT * FROM foo;", a)).params == a }
+  property("params are set properly") = forAll(legitimateString, alphaMap) { (q: String, a: Map[String,ParameterValue[_]]) => (new Query(q, a)).params == a }
 
-  property("addition preserves parameters, no intersection") = forAll(alphaMap, alphaMap) { (a: Map[String,String], b: Map[String,String]) => {
-    val q1 = "SELECT * FROM foo ".sqlP(a)
-    val q2 = "WHERE bar;".sqlP(b -- a.keySet) //Make sure a and b have no duplicated elements
-    (q1 |+| q2).params == (a ++ b)
+  property("addition preserves parameters, no intersection") = forAll(alphaMap, alphaMap) { (a: Map[String,ParameterValue[_]], b: Map[String,ParameterValue[_]]) => {
+    val (ac, bc) = noDupes(a, b)
+    val q1 = "SELECT * FROM foo ".sqlM(ac)
+    val q2 = "WHERE bar;".sqlM(bc) //Make sure a and b have no duplicated elements
+    ((q1 |+| q2).params == (ac ++ bc))
   } }
 
-  property("addition preserves parameters, intersection has same keys and values") = forAll(alphaMap, alphaMap, legitimateString, legitimateString) { (a: Map[String,String], b: Map[String,String], c: String, d: String) => {
-    val q1 = "SELECT * FROM foo ".sqlP((a - c) + (c -> d))
-    val q2 = "WHERE bar;".sqlP((b -- a.keySet - c) + (c -> d)) //Make sure a and b have no duplicated elements
-    (q1 |+| q2).params == (a ++ b + (c -> d))
+  property("addition preserves parameters, intersection has same keys and values") = forAll(legitimateString, legitimateString, alphaMap, alphaMap) { (c: String, d: String, a: Map[String,ParameterValue[_]], b: Map[String,ParameterValue[_]]) => {
+    val (ac, bc) = noDupes(a - c, b - c)
+    val q1 = "SELECT * FROM foo ".sqlM(ac + (c -> d))
+    val q2 = "WHERE bar;".sqlM(bc + (c -> d)) //Make sure a and b have no duplicated elements
+    (c != "") ==> ((q1 |+| q2).params == (ac ++ bc + (c -> d)))
   } }
 
   property("sqlV generates parameter objects") = forAll(legitimateString) { (b: String) => {
-    val r = b.sqlV
-    r.params.values.toList == List(b)
+    val r = toParameterValue(b).sqlV
+    r.params.values.toList.size == 1
   } }
 
   property("sqlV generates parameter objects, addition works") = forAll(legitimateString, legitimateString) { (a: String, b: String) => {
-    val r = a.sql |+| b.sqlV
+    val r = a.sql |+| toParameterValue(b).sqlV
     val paramKey = r.params.keySet.head // An arbitrarily chosen parameter for b
     (r.params.size == 1) && (r.params.contains(paramKey)) && r.sql.endsWith("{%s}".format(paramKey))
   } }
@@ -73,9 +90,9 @@ object QueryErrorSpecification extends Properties("Query.ErrorChecks") {
 
   import Generators._
 
-  property("throws error when parameter key is duplicated, values different") = forAll(legitimateString, alphaMap, legitimateString, legitimateString) { (a: String, b: Map[String,String], c: String, d: String) => {
-    val q1 = a.sqlP(b + (c -> d))
-    val q2 = a.sqlP(b + (c -> (d + "_suffix")))
+  property("throws error when parameter key is duplicated, values different") = forAll(legitimateString, alphaMap, legitimateString, legitimateString) { (a: String, b: Map[String,ParameterValue[_]], c: String, d: String) => {
+    val q1 = a.sqlM(b + (c -> d))
+    val q2 = a.sqlM(b + (c -> (d + "_suffix")))
     throws({q1 |+| q2}, classOf[DuplicatedParameterException])
   } }
 
