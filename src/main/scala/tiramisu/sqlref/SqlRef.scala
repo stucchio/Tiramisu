76ef3b6564daae5270d073939ad5c3f41d985b23
Chris Stucchio <stucchio@gmail.com>
2014-01-03 07:46:49 -0500

    Created constructsAllRefs trait

--- src/main/scala/tiramisu/sqlref/SqlRef.scala
+++ src/main/scala/tiramisu/sqlref/SqlRef.scala
@@ -13,3 +13,14 @@ trait PolymorphicSingleTableRefType[T] extends PolymorphicRefType[T] {
   def rowIdentifyingClause(ref: T): Query
   def pkClause(ref: T): Query = ("(SELECT " + table.pkFieldName + " FROM " + table.tableName + " WHERE ").sqlP() + rowIdentifyingClause(ref) + ")".sqlP()
 }
+
+trait ConstructsAllRefs[B,T] {
+  // Represents a class which can be used to construct ALL references to the same object.
+  //
+  // For example, consider a table foo. The class FooFull(pk: Long, slug: String) represents a full row from this table.
+  // There is a FooRef class (having subclasses FooRefByPrimaryKey(Long) and FooRefBySlug(String)
+  //
+  // A ConstructsAllRefs[B,T] object would satisfy:
+  //    allRefs(fooFull) == Seq(FooRefByLong(fooFull.pk), FooRefBySlug(fooFull.slug))
+  def allRefs(b: B): Seq[T]
+}
